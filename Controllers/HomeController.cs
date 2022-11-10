73b33c0adc9b9d49bf5f93e49ce312154c1d5b56
Ficiao <filipmtvn@gmail.com>
2022-11-10 19:59:56 +0100

    Finished implementation of victim server

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,134 @@
+﻿using Lab2VictimServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Web;
+using System.Diagnostics;
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Lab2VictimServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string name = Request.Cookies["LoginName"];
+            string pass = Request.Cookies["LoginPass"];
+            string balance = Request.Cookies["Balance"];
+            
+            if(name != null)
+            {
+                ViewBag.AccountData = new BankAccount()
+                {
+                    AccountName = name,
+                    AccountPass = pass,
+                    Balance = Int32.Parse(balance)
+                };
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string accountName, string accountPass)
+        {
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTimeOffset.UtcNow.AddDays(1)
+            };
+            Response.Cookies.Append("LoginName", accountName, options);
+            Response.Cookies.Append("LoginPass", accountPass, options);
+            Response.Cookies.Append("Balance", "1000", options);
+
+            ViewBag.AccountData = new BankAccount()
+            {
+                AccountName = accountName,
+                AccountPass = accountPass,
+                Balance = 1000
+            };
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SafeBalanceChange(float changeAmmount, string recieverName)
+        {
+            string name = Request.Cookies["LoginName"];
+            string pass = Request.Cookies["LoginPass"];
+            string balance = Request.Cookies["Balance"];
+
+            if (name != null)
+            {
+                int newValue = Int32.Parse(balance) - (int)changeAmmount;
+                ViewBag.AccountData = new BankAccount()
+                {
+                    AccountName = name,
+                    AccountPass = pass,
+                    Balance = Int32.Parse(balance) - (int)changeAmmount
+                };
+
+                ViewBag.Reciever = recieverName;
+                ViewBag.RecieveAmmount = changeAmmount;
+
+                CookieOptions options = new CookieOptions()
+                {
+                    Expires = DateTimeOffset.UtcNow.AddDays(1)
+                };
+                Response.Cookies.Delete("Balance");
+                Response.Cookies.Append("Balance", newValue.ToString(), options);
+            }
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult VolunerableBalanceChange(float changeAmmount, string recieverName)
+        {
+            string name = Request.Cookies["LoginName"];
+            string pass = Request.Cookies["LoginPass"];
+            string balance = Request.Cookies["Balance"];
+
+            if (name != null)
+            {
+                int newValue = Int32.Parse(balance) - (int)changeAmmount;
+                ViewBag.AccountData = new BankAccount()
+                {
+                    AccountName = name,
+                    AccountPass = pass,
+                    Balance = Int32.Parse(balance) - (int)changeAmmount
+                };
+
+                ViewBag.Reciever = recieverName;
+                ViewBag.RecieveAmmount = changeAmmount;
+
+                CookieOptions options = new CookieOptions()
+                {
+                    Expires = DateTimeOffset.UtcNow.AddDays(1)
+                };
+                Response.Cookies.Delete("Balance");
+                Response.Cookies.Append("Balance", newValue.ToString(), options);
+            }
+
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2VictimServer.Models
+{
+    public class BankAccount
+    {
+        public string AccountName;
+        public string AccountPass;
+        public int Balance;
+    }
+}
